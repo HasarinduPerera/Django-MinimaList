4f3cd4077d967bd41f1ab17828429242c8e8159b
Hasarindu Perera <38999847+HasarinduPerera@users.noreply.github.com>
2022-01-20 04:52:14 +0530

    Boilerplate and Base

---
+++ base/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
+
+# Create your views here.
+class TaskList(ListView):
+    model = Task
+    #template_name = "task_list.html"
+    context_object_name = 'tasks'
+
+
