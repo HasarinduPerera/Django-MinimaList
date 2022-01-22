1b65b8585f3f9e0035e89e681f7a8f0339b88032
Hasarindu Perera <38999847+HasarinduPerera@users.noreply.github.com>
2022-01-23 01:18:26 +0530

    Create Delete View and User Auth

--- base/views.py
+++ base/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.contrib.auth.views import LoginView
+
 from django.urls import reverse_lazy
 
 from .models import Task
 
 # Create your views here.
+class UserLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
 class TaskList(ListView):
     model = Task
     template_name = "base/task_list.html"
@@ -28,4 +40,10 @@ class TaskUpdate(UpdateView):
     model = Task
     #template_name = "base/update_form.html"
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
