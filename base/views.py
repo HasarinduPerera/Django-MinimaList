fa22775b86a7a32f8fe31f51fcb956fba57afc45
Hasarindu Perera <38999847+HasarinduPerera@users.noreply.github.com>
2022-01-23 00:13:47 +0530

    Create and Update Views

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+
 from .models import Task
 
 # Create your views here.
@@ -14,3 +17,15 @@ class TaskDetail(DetailView):
     model = Task
     template_name = "base/task_detail.html"
     context_object_name = 'tasks'
+
+class TaskCreate(CreateView):
+    model = Task
+    template_name = "base/task_form.html"
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    #template_name = "base/update_form.html"
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
